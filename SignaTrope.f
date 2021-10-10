51b5026b66803c5b5bb7107f8397fa4a3506d88a
SilverZero <49707789+d6rks1lv3rz3r0@users.noreply.github.com>
2021-10-10 01:29:14 -0500

    Update SignaTrope.f

--- SignaTrope.f
+++ SignaTrope.f
@@ -1,146 +1,105 @@
-// Stabilization - East
-FD=abs(:F1S-:O1S) UD=abs(:U1S-:O1S) i=(FD<2.5)*(UD<2.5) :Chk1=i
-if :Chk1*:Chk2*:Chk3 then :O1=:O1S :F1=:F1S :U1=:U1S end goto1
+// --------------------------Synchronized-------------------------- //
 
-// Stabilization - West
-FD=abs(:F2S-:O2S) UD=abs(:U2S-:O2S) i=(FD<2.5)*(UD<2.5) :Chk2=i 
-if :Chk1*:Chk2*:Chk3 then :O2=:O2S :F2=:F2S :U2=:U2S end goto1
+//    Memory 1    //    Memory 2    //
+// cw0            // cw1            //
+// cw1            // cw2            //
+// cw2            // cw3            //
+// cw3            // cw4            //
+// cw4            // cw5            //
+// cw5            // Speed          //
+// Speed          // Delta          //
+// Delta          // Stabilization  //
+// Stabilization  // FlushReceivers //
+// FlushReceivers // cw-            //
 
-// Stabilization - North
-FD=abs(:F3S-:O3S) UD=abs(:U3S-:O3S) i=(FD<2.5)*(UD<2.5) :Chk3=i
-if :Chk1*:Chk2*:Chk3 then :O3=:O3S :F3=:F3S :U3=:U3S end goto1
+// Scheduler Start - Start
+:cw0=0 goto1
 
-// Stabilization - Origin Synchronization 
-:Nav=:Chk1*:Chk2*:Chk3 goto1
+// Scheduler Stop - cw0
+:cw0=-1 goto1
 
-// Trilateration - O
+// Flush Receivers - FlushReceivers
+:TargetFrequency=2 :TargetFrequency=1 goto1
+
+// Stabilization - Stabilization
+:O1=:O :F1=:F :U1=:U :O2=:P :F2=:G :U2=:V :O3=:Q :F3=:H :U3=:W goto1
+
+// Trilateration Delta - Delta
 O=241868 P=14625000000 Q=128996 R=145121 M=1000000 T=9425000000 h=0.5
-A=M-:O3 B=M-:BS C=M-:O1 D=M-:O2 N=A*A S=B*B E=C*C W=D*D K=:ZSg
-:OX=(E-W+P)/O :OY=(E-N+T-Q*:OX)/R :OZ=K*(E-:OX^2-:OY^2)^h goto(3-:Nav)
+N=M-:O3 E=M-:O1 W=M-:O2 N*=N E*=E W*=W :DX=X-A :DY=Y-B :DZ=Z-C
+A=X B=Y C=Z X=(E-W+P)/O Y=(E-N+T-Q*X)/R Z=:ZSg*(E-X^2-Y^2)^h goto2
 
-// Trilateration - F
-O=241868 P=14625000000 Q=128996 R=145121 M=1000000 T=9425000000 h=0.5
-A=M-:F3 B=M-:BS C=M-:F1 D=M-:F2 N=A*A S=B*B E=C*C W=D*D K=:ZSg
-:FX=(E-W+P)/O :FY=(E-N+T-Q*:FX)/R :FZ=K*(E-:FX^2-:FY^2)^h goto(3-:Nav)
+// Speed - Speed
+:vf=X*:fi+Y*:fj+Z*:fk :vu=X*:ui+Y*:uj+Z*:uk :vr=X*:ri+Y*:rj+Z*:rk 
+d=0.6 X=:DX/d Y=:DY/d Z=:DZ/d goto1
+
+// --------------------------Synchronized-------------------------- //
+
+// --------------------------Asynchronized------------------------- //
+
+// Signal Stability Indicator
+t=2.5^2 A=:F1-:O1 B=:U1-:O1 C=:F2-:O2 D=:U2-:O2 E=:F3-:O3 F=:U3-:O3
+:Nav=(A^2<t)*(B^2<t)*(C^2<t)*(D^2<t)*(E^2<t)*(F^2<t) goto1
+
+// Coordinate Stability Indicator
+A=abs(:D4) B=abs(:D) C=abs(:D5) D=abs(:D2) E=abs(:D6) F=abs(:D3) 
+t=2.5 :Cord=(A<t)*(B<t)*(C<t)*(D<t)*(E<t)*(F<t)
+
+// Orientation Stability Indicator
+:Ori=abs(:ui*:fi+:uj*:fj+:uk*:fk)<0.087 goto1
 
 // Trilateration - U
 O=241868 P=14625000000 Q=128996 R=145121 M=1000000 T=9425000000 h=0.5
-A=M-:U3 B=M-:BS C=M-:U1 D=M-:U2 N=A*A S=B*B E=C*C W=D*D K=:ZSg
-:UX=(E-W+P)/O :UY=(E-N+T-Q*:UX)/R :UZ=K*(E-:UX^2-:UY^2)^h goto(3-:Nav)
+N=M-:U3 S=M-:BS E=M-:U1 W=M-:U2 N*=N S*=S E*=E W*=W K=:ZSg
+:UX=(E-W+P)/O :UY=(E-N+T-Q*:UX)/R :UZ=K*(E-:UX^2-:UY^2)^h 
+goto2
+
+// Trilateration - F
+O=241868 P=14625000000 Q=128996 R=145121 M=1000000 T=9425000000 h=0.5
+N=M-:F3 S=M-:BS E=M-:F1 W=M-:F2 N*=N S*=S E*=E W*=W K=:ZSg
+:FX=(E-W+P)/O :FY=(E-N+T-Q*:FX)/R :FZ=K*(E-:FX^2-:FY^2)^h
+goto2
+
+// Trilateration - O
+O=241868 P=14625000000 Q=128996 R=145121 M=1000000 T=9425000000 h=0.5
+N=M-:O3 S=M-:BS E=M-:O1 W=M-:O2 N*=N S*=S E*=E W*=W K=:ZSg
+:X=(E-W+P)/O :Y=(E-N+T-Q*:X)/R :Z=K*(E-:X^2-:Y^2)^h 
+:D=:UX-:X :D2=:UY-:Y :D3=:UZ-:Z :D4=:FX-:X :D5=:FY-:Y :D6=:FZ-:Z goto2
 
 // Trilateration - Reflection
 G=(66359-:X)^2+(-37279-:Y)^2 R=(1000000-:BS) n=71111 :ZSg=1/(:ZSg==0)
 A=abs(R-(G+(n-abs(:Z))^2)^.5) B=abs(R-(G+(n+abs(:Z))^2)^.5)
 :ZSg = B>A - A>B goto1 
 
-// Stabilization - X
-FD=abs(:FX-:OX) UD=abs(:UX-:OX) i=(FD<2.5)*(UD<2.5) :Ch1=i goto1
+// Orientation - U and F
+h=.5
+X=:D Y=:D2 Z=:D3 N=(X^2+Y^2+Z^2)^h A=:D4 B=:D5 C=:D6 M=(A^2+B^2+C^2)^h
+:ui=X/N :uj=Y/N :uk=Z/N :fi=A/M :fj=B/M :fk=C/M goto2
 
-// Stabilization - Y
-FD=abs(:FY-:OY) UD=abs(:UY-:OY) i=(FD<2.5)*(UD<2.5) :Ch2=i goto1
-
-// Stabilization - Z
-FD=abs(:FZ-:OZ) UD=abs(:UZ-:OZ) i=(FD<2.5)*(UD<2.5) :Ch3=i goto1
-
-// Stabilization - Coordinate Synchronization 
-:Nav2=:Ch1*:Ch2*:Ch3 if :Nav2 then :X=:OX :Y=:OY :Z=:OZ end goto1
-
-// Normalization - U
-X=:UX-:OX Y=:UY-:OY Z=:UZ-:OZ c=(X<2)*(Y<2)*(Z<2) N=(X^2+Y^2+Z^2)^.5
-if c then :ui=X/N :uj=Y/N :uk=Z/N end goto1
-
-// Normalization - F
-X=:FX-:OX Y=:FY-:OY Z=:FZ-:OZ c=(X<2)*(Y<2)*(Z<2) N=(X^2+Y^2+Z^2)^.5
-if c then :fi=X/N :fj=Y/N :fk=Z/N end goto1
-
-// Normalization - R
+// Orientation - R
 :ri=:fj*:uk-:fk*:uj :rj=:fk*:ui-:fi*:uk :rk=:fi*:uj-:fj*:ui goto1
 
-// Translation - XYZ
-A=:X-X B=:Y-Y C=:Z-Z X=:X Y=:Y Z=:Z d++ goto(1+((A+B+C)!=0))
-d+=d%2 :vX=A/d/0.2 :vY=B/d/0.2 :vZ=C/d/0.2 d=2 :DX=A :DY=B :DZ=C goto1
-
-// Stabilization - vF
-i=3
-:vF=(S1+S2+S3+S4+S5)/5000*1000 :vFv=(D1+D2+D3+D4+D5)/5000*1000 gotoi
-S1=:vX*:fi+:vY*:fj+:vZ*:fk i++ D1=abs(:vF-S1) goto2
-S2=:vX*:fi+:vY*:fj+:vZ*:fk i++ D2=abs(:vF-S2) goto2
-S3=:vX*:fi+:vY*:fj+:vZ*:fk i++ D3=abs(:vF-S3) goto2
-S4=:vX*:fi+:vY*:fj+:vZ*:fk i++ D4=abs(:vF-S4) goto2
-S5=:vX*:fi+:vY*:fj+:vZ*:fk i=3 D5=abs(:vF-S5) goto2
-
-// Stabilization - vU
-i=3
-:vu=(S1+S2+S3+S4+S5)/5000*1000 :vuv=(D1+D2+D3+D4+D5)/5000*1000 gotoi
-S1=:vX*:ui+:vY*:uj+:vZ*:uk i++ D1=abs(:vu-S1) goto2
-S2=:vX*:ui+:vY*:uj+:vZ*:uk i++ D2=abs(:vu-S2) goto2
-S3=:vX*:ui+:vY*:uj+:vZ*:uk i++ D3=abs(:vu-S3) goto2
-S4=:vX*:ui+:vY*:uj+:vZ*:uk i++ D4=abs(:vu-S4) goto2
-S5=:vX*:ui+:vY*:uj+:vZ*:uk i=3 D5=abs(:vu-S5) goto2
-
-// Stabilization - vR
-i=3
-:vr=(S1+S2+S3+S4+S5)/5000*1000 :vrv=(D1+D2+D3+D4+D5)/5000*1000 gotoi
-S1=:vX*:ri+:vY*:rj+:vZ*:rk i++ D1=abs(:vr-S1) goto2
-S2=:vX*:ri+:vY*:rj+:vZ*:rk i++ D2=abs(:vr-S2) goto2
-S3=:vX*:ri+:vY*:rj+:vZ*:rk i++ D3=abs(:vr-S3) goto2
-S4=:vX*:ri+:vY*:rj+:vZ*:rk i++ D4=abs(:vr-S4) goto2
-S5=:vX*:ri+:vY*:rj+:vZ*:rk i=3 D5=abs(:vr-S5) goto2
-
-// Stabilization - u
-i=4
-:uia=(A1+A2+A3)/30*10 :uja=(B1+B2+B3)/30*10 :uka=(C1+C2+C3)/30*10
-:uiad=(D1+D2+D3)/3 :ujad=(E1+E2+E3)/3 :ukad=(F1+F2+F3)/3 gotoi
-A1=:ui B1=:uj C1=:uk D1=:uia-A1 E1=:uja-B1 F1=:uka-C1 i++ goto2
-A2=:ui B2=:uj C2=:uk D2=:uia-A2 E2=:uja-B2 F2=:uka-C2 i++ goto2
-A3=:ui B3=:uj C3=:uk D3=:uia-A3 E3=:uja-B3 F3=:uka-C3 i=4 goto2
-
-// Stabilization - f
-i=4
-:fia=(A1+A2+A3)/30*10 :fja=(B1+B2+B3)/30*10 :fka=(C1+C2+C3)/30*10
-:fiad=(D1+D2+D3)/3 :fjad=(E1+E2+E3)/3 :fkad=(F1+F2+F3)/3 gotoi
-A1=:fi B1=:fj C1=:fk D1=:fia-A1 E1=:fja-B1 F1=:fka-C1 i++ goto2
-A2=:fi B2=:fj C2=:fk D2=:fia-A2 E2=:fja-B2 F2=:fka-C2 i++ goto2
-A3=:fi B3=:fj C3=:fk D3=:fia-A3 E3=:fja-B3 F3=:fka-C3 i=4 goto2
-
-// Stabilization - r
-i=4
-:ria=(A1+A2+A3)/30*10 :rja=(B1+B2+B3)/30*10 :rka=(C1+C2+C3)/30*10
-:riad=(D1+D2+D3)/3 :rjad=(E1+E2+E3)/3 :rkad=(F1+F2+F3)/3 gotoi
-A1=:ri B1=:rj C1=:rk D1=:ria-A1 E1=:rja-B1 F1=:rka-C1 i++ goto2
-A2=:ri B2=:rj C2=:rk D2=:ria-A2 E2=:rja-B2 F2=:rka-C2 i++ goto2
-A3=:ri B3=:rj C3=:rk D3=:ria-A3 E3=:rja-B3 F3=:rka-C3 i=4 goto2
-
-// Synchronization - u
-t=0.05
-:Ori1=(abs(:uiad)<t)*(abs(:ujad)<t)*(abs(:ukad)<t) goto2
-
-// Synchronization - f
-t=0.05
-:Ori2=(abs(:fiad)<t)*(abs(:fjad)<t)*(abs(:fkad)<t) goto2
-
-// Synchronization - r
-t=0.05
-:Ori3=(abs(:riad)<t)*(abs(:rjad)<t)*(abs(:rkad)<t) goto2
-
-// Synchronization - ufr
-:Ori=:Ori1*:Ori2*:Ori3 goto1
-
-// WayPoint - Distance
+// Direction - Distance
 :DWPx=:WPCX-:X :DWPy=:WPCY-:Y :DWPz=:WPCZ-:Z h=.5
 :DWPD=(:DWPx^2+:DWPx^2+:DWPx^2)^h goto1
 
-// WayPoint - Projection
-R=x*:ria+y*:rja+z*:rka F=x*:fia+y*:fja+z*:fka U=x*:uia+y*:uja+z*:uka
-x=:DWPx y=:DWPy z=:DWPz if :Ori then :WPCR=R :WPCF=F :WPCU=U end goto1
+// Direction - Projection
+x=:DWPx y=:DWPy z=:DWPz :WPCR=R :WPCF=F :WPCU=U
+R=x*:ri+y*:rj+z*:rk F=x*:fi+y*:fj+z*:fk U=x*:ui+y*:uj+z*:uk goto1
 
-// WayPoint - Yaw
-k=R>0-0>R j=1/3 c=i*(2-(2+2*a)^h)^h-j*(2-2*a)^h :Yaw=k*c*b/:Ori 
-R=:WPCR F=:WPCF a=(F^2+R^2)^h a=F/a h=.5 b=57.296 i=8/3 goto1  
-  
-// WayPoint - Pitch
-k=t^2>1 m=1-2*(t<0) t^=1-2*k t*=b/(1+i*t^2) :Pit=m*k*90+(k<1-k)*t/:Ori 
-R=:WPCR F=:WPCF U=:WPCU h=.5 b=57.296 i=0.281 t=U/(F^2+R^2)^h goto1
+// Direction - Yaw
+h=.5 b=57.296 i=8/3 j=1/3
+R=:WPCR F=:WPCF a=(F^2+R^2)^h a=F/a k=R>0-0>R
+c=i*(2-(2+2*a)^h)^h-j*(2-2*a)^h :Yaw=k*c*b goto2
+
+// Direction - Pitch
+h=.5 b=57.296 i=0.281
+R=:WPCR F=:WPCF U=:WPCU  t=U/(F^2+R^2)^h k=t^2>1 m=1-2*(t<0)
+t^=1-2*k t*=b/(1+i*t^2) :Pit=m*k*90+(k<1-k)*t goto2
+
+// --------------------------Asynchronized------------------------- //
+
+// -------------------------Display Drivers------------------------ //
 
 // Display - Location
 h="Signa :1  : Trope :   : Coord :"
@@ -151,14 +110,14 @@ h="Signa :2  : Trope :   : Delta :"
 :2=h+"\nDX:"+(:DX/10*10)+"\nDY:"+(:DY/10*10)+"\nDZ:"+(:DZ/10*10) goto1
 
 // Display - Speed
-S="Signa :3  : Trope :   : Speed : "+"\nFB:"+:vF+"±"+:vFv
-:3=S+"\nRL:"+:vR+"±"+:vRv+"\nUD:"+:vU+"±"+:vUv+"\n    m/s" goto1
+S="Signa :3  : Trope :   : Speed : "+"\nFB:"+:vF
+:3=S+"\nRL:"+:vR+"\nUD:"+:vU+"\n    m/s" goto1
 
 // Display - WayPoint
 a=1000 h="Signa :4  : Trope :   : Dest. :"+"\nDX:"+(:DWPx/a*a)+"\nDY:"
 :4=h+(:DWPy/a*a)+"\nDZ:"+(:DWPz/a*a)+"\nD:"+(:DWPD/a*a)+" m" goto1
 
-// Display - Compass Horizontal
+// Display - CrossHair Horizontal
 s=" " c="×"
 i=(:Yaw/10)/1000*1000 if i>4 then i=4 end if i<-4 then i=-4 endgotoi+7
 :horcom=c+s+s+s+s+s+s+s+s :navcentered=0 goto2
@@ -171,7 +130,7 @@ p=c if :verctr then p="※" end  :horcom=s+s+s+s+p+s+s+s+s goto2
 :horcom=s+s+s+s+s+s+s+c+s goto2
 :horcom=s+s+s+s+s+s+s+s+c goto2
 
-// Display - Compass Vertical
+// Display - CrossHair Vertical
 t="  ¦   :0 " b="0:   ¦   :0 " l="            " m="-    +    - "
 j=(:Pit/10)/1000*1000 if j>2 then j=2 end if j<-2 then j=-2 endgotoj+5
 c=" "+:horcom+"  " :0=t+l+l+m+l+c+b :verctr=0 goto2
@@ -179,3 +138,5 @@ c=" "+:horcom+"  " :0=t+l+l+m+c+l+b :verctr=0 goto2
 c="-"+:horcom+"- " :0=t+l+l+c+l+l+b :verctr=1 goto2
 c=" "+:horcom+"  " :0=t+l+c+m+l+l+b :verctr=0 goto2
 c=" "+:horcom+"  " :0=t+c+l+m+l+l+b :verctr=0 goto2
+
+// -------------------------Display Drivers------------------------ //
